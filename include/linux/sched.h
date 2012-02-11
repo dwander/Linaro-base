0acfbfaee99fd731db9c80f6f17225d0f97ebb24
Peter Zijlstra <peterz@infradead.org>
2012-02-11 06:05:00 +0100

    sched/fair: Track cgroup depth
    Track depth in cgroup tree, this is useful for things like
    find_matching_se() where you need to get to a common parent of two
    sched entities.
    
    Keeping the depth avoids having to calculate it on the spot, which
    saves a number of possible cache-misses.
    
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/1328936700.2476.17.camel@laptop
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit fed14d45f945042a15b09de48d7d3d58d9455fc4)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -1064,6 +1064,7 @@ struct sched_entity {
 #endif
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
+	int			depth;
 	struct sched_entity	*parent;
 	/* rq on which this entity is (to be) queued: */
 	struct cfs_rq		*cfs_rq;
