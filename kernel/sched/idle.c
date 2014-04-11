de799fd61c8cb7b8957218500b971a85b5307da5
Peter Zijlstra <peterz@infradead.org>
2014-04-11 13:47:16 +0200

    sched/idle: Delay clearing the polling bit
    With the generic idle functions assuming !polling we should only clear
    the polling bit at the very last opportunity in order to avoid
    spurious IPIs.
    
    Ideally we'd flip the default to polling, but that means auditing all
    arch idle functions.
    
    Signed-off-by: Peter Zijlstra <peterz@infradead.org>
    Acked-by: Nicolas Pitre <nicolas.pitre@linaro.org>
    Cc: Daniel Lezcano <daniel.lezcano@linaro.org>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Link: http://lkml.kernel.org/n/tip-vq7719foqzf6z5h4j7eh7f9e@git.kernel.org
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit c444117f0f39d59733ec23da67c44424df529230)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>
    
    Conflicts:
    	kernel/sched/idle.c

--- kernel/sched/idle.c
+++ kernel/sched/idle.c
@@ -78,12 +78,10 @@ static int cpuidle_idle_call(void)
 
 	/*
 	 * Check if the idle task must be rescheduled. If it is the
-	 * case, exit the function after re-enabling the local irq and
-	 * set again the polling flag
+	 * case, exit the function after re-enabling the local irq.
 	 */
-	if (current_clr_polling_and_test()) {
+	if (need_resched()) {
 		local_irq_enable();
-		__current_set_polling();
 		return 0;
 	}
 
@@ -121,7 +119,7 @@ static int cpuidle_idle_call(void)
 			broadcast = !!(drv->states[next_state].flags &
 				       CPUIDLE_FLAG_TIMER_STOP);
 
-			if (broadcast)
+			if (broadcast) {
 				/*
 				 * Tell the time framework to switch
 				 * to a broadcast timer because our
@@ -133,6 +131,7 @@ static int cpuidle_idle_call(void)
 				ret = clockevents_notify(
 					CLOCK_EVT_NOTIFY_BROADCAST_ENTER,
 					&dev->cpu);
+			}
 
 			if (ret >= 0) {
 				trace_cpu_idle_rcuidle(next_state, dev->cpu);
@@ -169,8 +168,12 @@ static int cpuidle_idle_call(void)
 	 * We can't use the cpuidle framework, let's use the default
 	 * idle routine
 	 */
-	if (ret < 0)
-		arch_cpu_idle();
+	if (ret) {
+		if (!current_clr_polling_and_test())
+			arch_cpu_idle();
+		else
+			local_irq_enable();
+	}
 
 	__current_set_polling();
 
