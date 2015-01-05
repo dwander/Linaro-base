bed25201031a133c6b79f00f2a1aa7f5cf2e7e7d
Peter Zijlstra <peterz@infradead.org>
2015-01-05 11:18:10 +0100

    sched/core: Validate rq_clock*() serialization
    rq->clock{,_task} are serialized by rq->lock, verify this.
    
    One immediate fail is the usage in scale_rt_capability, so 'annotate'
    that for now, there's more 'funny' there. Maybe change rq->lock into a
    raw_seqlock_t?
    
    (Only 32-bit is affected)
    
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: http://lkml.kernel.org/r/20150105103554.361872747@infradead.org
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: umgwanakikbuti@gmail.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    (cherry picked from commit cebde6d681aa45f96111cfcffc1544cf2a0454ff)
    Signed-off-by: Alex Shi <alex.shi@linaro.org>
    
     Conflicts:
    	kernel/sched/fair.c

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5663,9 +5663,12 @@ static unsigned long scale_rt_capacity(int cpu)
 	 */
 	age_stamp = ACCESS_ONCE(rq->age_stamp);
 	avg = ACCESS_ONCE(rq->rt_avg);
+	delta = __rq_clock_broken(rq) - age_stamp;
 
-	total = sched_avg_period() + (rq_clock(rq) - age_stamp);
+	if (unlikely(delta < 0))
+		delta = 0;
 
+	total = sched_avg_period() + delta;
 	used = div_u64(avg, total);
 
 	if (likely(used < SCHED_CAPACITY_SCALE))
