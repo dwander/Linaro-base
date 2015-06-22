e00661e2d2b38d579aa980c7794f4f40a6a400da
Patrick Bellasi <patrick.bellasi@arm.com>
2015-06-22 16:44:48 +0100

    WIP: sched/{fair,tune}: add per task boost signal
    When per task boosting is enabled, all the CPUs and Task specific
    signals must be boosted according to the specific boost value defined by
    the boost group assigned to the task.
    
    This patch updates all the CFS scheduler consumers, for the task
    "utilization" signal and the CPU "usage" signal, to use the boost value
    defined by the boost group assigned to a task.
    
    Change-Id: I4348b387d66a7a8f458deca426c928d0341cf7a6
    Signed-off-by: Patrick Bellasi <patrick.bellasi@arm.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -4814,6 +4814,7 @@ struct energy_env {
 	int			dst_cpu;
 	int			energy;
 	int			energy_payoff;
+	struct task_struct	*task;
 	struct {
 		int before;
 		int after;
@@ -5038,7 +5039,11 @@ static int energy_diff_evaluate(struct energy_env *eenv)
 	int nrg_delta;
 
 	/* Return energy diff when boost margin is 0 */
+#ifdef CONFIG_CGROUP_SCHEDTUNE
+	boost = schedtune_taskgroup_boost(eenv->task);
+#else
 	boost = get_sysctl_sched_cfs_boost();
+#endif
 	if (boost == 0)
 		return eenv->nrg.diff;
 
@@ -5281,7 +5286,11 @@ schedtune_task_margin(struct task_struct *task)
 	unsigned long utilization;
 	unsigned long margin;
 
+#ifdef CONFIG_CGROUP_SCHEDTUNE
+	boost = schedtune_taskgroup_boost(task);
+#else
 	boost = get_sysctl_sched_cfs_boost();
+#endif
 	if (boost == 0)
 		return 0;
 
@@ -5416,6 +5425,7 @@ static int energy_aware_wake_cpu(struct task_struct *p, int target)
 			.usage_delta	= task_utilization(p),
 			.src_cpu	= task_cpu(p),
 			.dst_cpu	= target_cpu,
+			.task		= p,
 		};
 
 		/* Not enough spare capacity on previous cpu */
@@ -5432,12 +5442,16 @@ static int energy_aware_wake_cpu(struct task_struct *p, int target)
 #ifdef CONFIG_SCHED_TUNE
 
 static inline unsigned int
-schedtune_cpu_margin(unsigned long usage)
+schedtune_cpu_margin(int cpu, unsigned long usage)
 {
 	unsigned int boost;
 	unsigned long margin;
 
+#ifdef CONFIG_CGROUP_SCHEDTUNE
+	boost = schedtune_cpu_boost(cpu);
+#else
 	boost = get_sysctl_sched_cfs_boost();
+#endif
 	if (boost == 0)
 		return 0;
 	margin = schedtune_margin(usage, boost);
@@ -5448,7 +5462,7 @@ schedtune_cpu_margin(unsigned long usage)
 #else /* CONFIG_SCHED_TUNE */
 
 static inline unsigned int
-schedtune_cpu_margin(unsigned long usage)
+schedtune_cpu_margin(int cpu, unsigned long usage)
 {
 	return 0;
 }
@@ -5462,7 +5476,7 @@ get_boosted_cpu_usage(int cpu)
 	unsigned long margin;
 
 	usage = get_cpu_usage(cpu);
-	margin = schedtune_cpu_margin(usage);
+	margin = schedtune_cpu_margin(cpu, usage);
 
 	usage += margin;
 	return usage;
