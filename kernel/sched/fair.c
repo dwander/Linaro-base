69020cf01fc700ddba9e30b2d5292d3c77f83aa8
dwander <dqstyle@gmail.com>
2015-09-29 04:28:31 +0900

    sched/fair.c: increase HMP_DATA_SYSFS_MAX

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -2229,9 +2229,9 @@ struct hmp_global_attr {
 };
 
 #ifdef CONFIG_HMP_FREQUENCY_INVARIANT_SCALE
-#define HMP_DATA_SYSFS_MAX 17
+#define HMP_DATA_SYSFS_MAX 22
 #else
-#define HMP_DATA_SYSFS_MAX 16
+#define HMP_DATA_SYSFS_MAX 21
 #endif
 
 struct hmp_data_struct {
