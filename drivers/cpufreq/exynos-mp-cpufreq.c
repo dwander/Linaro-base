5c3425e9404c8488762794c66132c57a8a83c64a
Park Ju Hyung <qkrwngud825@gmail.com>
2015-10-31 16:41:46 +0900

    cafactive: fix compilation under this source tree
    interactive and cafactive cannot co-exist.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- drivers/cpufreq/exynos-mp-cpufreq.c
+++ drivers/cpufreq/exynos-mp-cpufreq.c
@@ -1911,6 +1911,10 @@ static int exynos_kfc_min_qos_handler(struct notifier_block *b, unsigned long va
 	threshold_freq = cpufreq_interactive_get_hispeed_freq(0);
 	if (!threshold_freq)
 		threshold_freq = 1000000;	/* 1.0GHz */
+#elif defined(CONFIG_CPU_FREQ_GOV_CAFACTIVE)
+	threshold_freq = cpufreq_cafactive_get_hispeed_freq(0);
+	if (!threshold_freq)
+		threshold_freq = 1000000;	/* 1.0GHz */
 #else
 	threshold_freq = 1000000;	/* 1.0GHz */
 #endif
@@ -2363,7 +2367,7 @@ err_alloc_info_CA7:
 	return ret;
 }
 
-#ifdef CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE
+#if defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE) || defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CAFACTIVE)
 device_initcall(exynos_cpufreq_init);
 #else
 late_initcall(exynos_cpufreq_init);
