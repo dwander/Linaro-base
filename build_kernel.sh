10dd364fa623567679c0a9bd1b09e88c4e4a3340
DQ <dqstyle@gmail.com>
2017-01-02 17:28:16 +0900

    Merge N916SKSU2DPK2
    Signed-off-by: DQ <dqstyle@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
+export CROSS_COMPILE=../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-eabi-4.8/bin/arm-eabi-
 
-make tre3calteskt_defconfig
+make exynos5433-tre3calteskt_defconfig
 
-make
+make -j64
