8b16fae0982045d308ed649988ab80531adb6e3c
Francisco Franco <franciscofranco.1990@gmail.com>
2017-05-22 15:54:24 -0400

    fs: fsync: add a toggle to enable or disable fsync() operations

--- fs/sync.c
+++ fs/sync.c
@@ -7,6 +7,7 @@
 #include <linux/fs.h>
 #include <linux/slab.h>
 #include <linux/export.h>
+#include <linux/module.h>
 #include <linux/namei.h>
 #include <linux/sched.h>
 #include <linux/writeback.h>
@@ -16,6 +17,9 @@
 #include <linux/quotaops.h>
 #include <linux/backing-dev.h>
 #include "internal.h"
+
+bool fsync_enabled = true;
+module_param(fsync_enabled, bool, 0755);
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
@@ -66,7 +70,7 @@ static struct workqueue_struct *intr_sync_wq;
 /* It prevents double allocation of intr_sync_wq */
 static DEFINE_MUTEX(intr_sync_wq_lock);
 
-static inline struct interruptible_sync_work *INTR_SYNC_WORK(struct work_struct *work) 
+static inline struct interruptible_sync_work *INTR_SYNC_WORK(struct work_struct *work)
 {
 	return container_of(work, struct interruptible_sync_work, work);
 }
@@ -85,7 +89,7 @@ static void do_intr_sync(struct work_struct *work)
 
 	dbg_print("\n[intr_sync] %s : call sys_sync on work[%d]-%ld\n",
 			__func__, sync_work->id, sync_work->version);
-	
+
 	/* if no one waits, do not call sync() */
 	if (waiter) {
 		ret = sys_sync();
@@ -161,7 +165,7 @@ find_idle:
 			queue_work(intr_sync_wq, &sync_work->work);
 		}
 		spin_unlock(&sync_work->lock);
-		
+
 		/* Return: 0 if timed out, and positive if completed. */
 		do {
 //			dbg_print("[intr_sync] waiting work[%d]\n", work_idx);
@@ -315,7 +319,7 @@ SYSCALL_DEFINE0(sync)
 	int nowait = 0, wait = 1;
 
 #ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
-	printk("sync logger: sync called by %s[%u] (tgid:%u)\n", 
+	printk("sync logger: sync called by %s[%u] (tgid:%u)\n",
 		current->comm, current->pid,  pid_vnr(task_tgid(current)));
 #endif
 	wakeup_flusher_threads(0, WB_REASON_SYNC);
@@ -327,7 +331,7 @@ SYSCALL_DEFINE0(sync)
 	if (unlikely(laptop_mode))
 		laptop_sync_completion();
 #ifndef CONFIG_SAMSUNG_PRODUCT_SHIP
-	printk("sync logger: sync done by %s[%u]\n", 
+	printk("sync logger: sync done by %s[%u]\n",
 		current->comm, current->pid);
 #endif
 	return 0;
@@ -371,6 +375,9 @@ SYSCALL_DEFINE1(syncfs, int, fd)
 	struct super_block *sb;
 	int ret;
 
+	if (!fsync_enabled)
+		return 0;
+
 	if (!f.file)
 		return -EBADF;
 	sb = f.file->f_dentry->d_sb;
@@ -406,6 +413,9 @@ SYSCALL_DEFINE1(syncfs, int, fd)
  */
 int vfs_fsync_range(struct file *file, loff_t start, loff_t end, int datasync)
 {
+	if (!fsync_enabled)
+		return 0;
+
 	if (!file->f_op->fsync)
 		return -EINVAL;
 	return file->f_op->fsync(file, start, end, datasync);
@@ -422,6 +432,9 @@ EXPORT_SYMBOL(vfs_fsync_range);
  */
 int vfs_fsync(struct file *file, int datasync)
 {
+	if (!fsync_enabled)
+		return 0;
+
 	return vfs_fsync_range(file, 0, LLONG_MAX, datasync);
 }
 EXPORT_SYMBOL(vfs_fsync);
@@ -430,6 +443,9 @@ static int do_fsync(unsigned int fd, int datasync)
 {
 	struct fd f = fdget(fd);
 	int ret = -EBADF;
+
+	if (!fsync_enabled)
+		return 0;
 
 	if (f.file) {
 		ret = vfs_fsync(f.file, datasync);
@@ -440,11 +456,17 @@ static int do_fsync(unsigned int fd, int datasync)
 
 SYSCALL_DEFINE1(fsync, unsigned int, fd)
 {
+	if (!fsync_enabled)
+		return 0;
+
 	return do_fsync(fd, 0);
 }
 
 SYSCALL_DEFINE1(fdatasync, unsigned int, fd)
 {
+	if (!fsync_enabled)
+		return 0;
+
 	return do_fsync(fd, 1);
 }
 
@@ -503,6 +525,9 @@ SYSCALL_DEFINE4(sync_file_range, int, fd, loff_t, offset, loff_t, nbytes,
 	struct address_space *mapping;
 	loff_t endbyte;			/* inclusive */
 	umode_t i_mode;
+
+	if (!fsync_enabled)
+		return 0;
 
 	ret = -EINVAL;
 	if (flags & ~VALID_FLAGS)
