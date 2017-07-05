1ba9b7a569a249eb95ce5ab9cad31bc1d0f2721d
Park Ju Hyung <qkrwngud825@gmail.com>
2017-07-05 23:45:19 +0900

    gpio_keys: directly boost CPU on wake input #2 Bypass INPUT_BOOSTER and boost directly for latency. Previous attempt failed because CPU cores were not ready to be onlined right after gpio input. We partially workarounded that with commit ef95d42f2196f12504df5ef44092d7b7fbc86574. Now take care of the scenario when the framebuffer is unblanking from non-deep-sleep state. Additional sanity check with !in_suspend_prepared is added to dm_cpu_hotplug. Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com> Signed-off-by: DQ <dqstyle@gmail.com>

--- drivers/input/keyboard/gpio_keys.c
+++ drivers/input/keyboard/gpio_keys.c
@@ -30,6 +30,7 @@
 #include <linux/of_gpio.h>
 #include <linux/of_irq.h>
 #include <linux/spinlock.h>
+#include <mach/cpufreq.h>
 
 #include <linux/sec_sysfs.h>
 #include <linux/sec_debug.h>
@@ -489,9 +490,16 @@ static void gpio_keys_gpio_report_event(struct gpio_button_data *bdata)
 	}
 
 	input_sync(input);
+
+	if (((button->code == KEY_POWER) || (button->code == KEY_HOMEPAGE))
+	    && !!state) {
+		event_hotplug_in();
+	}
 #ifdef CONFIG_INPUT_BOOSTER
 	if (button->code == KEY_HOMEPAGE)
 		input_booster_send_event(BOOSTER_DEVICE_KEY, !!state);
+ 	if ((button->code == KEY_RECENT) || (button->code == KEY_BACK))
+ 		input_booster_send_event(BOOSTER_DEVICE_TOUCHKEY, !!state);
 #endif
 }
 
